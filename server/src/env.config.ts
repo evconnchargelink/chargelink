3379c30f763e59f63e5acb3e78a1e22a34cfc529
ronak-pal1 <ronakpaul882@gmail.com>
2025-11-19 15:22:24 +0530

    email template using resend  added in server

--- server/src/env.config.ts
+++ server/src/env.config.ts
@@ -14,4 +14,5 @@ export const config = {
   MONGO_URI: process.env.MONGO_URI,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  RESEND: process.env.RESEND,
 };
