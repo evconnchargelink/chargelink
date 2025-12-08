3894d2156b3c402802f896a0ca4fdef15fbeddc3
ronak-pal1 <ronakpaul882@gmail.com>
2025-12-08 22:28:13 +0530

    removed iframe for test

--- web/src/pages/driver/Test.tsx
+++ web/src/pages/driver/Test.tsx
@@ -1,10 +1,10 @@
 const Test = () => {
   return (
     <div className="w-full h-full">
-      <iframe
+      {/* <iframe
         src="https://charge-link.base44.app/community"
         className="w-full h-[calc(100vh+20px)] -mt-20"
-      />
+      /> */}
     </div>
   );
 };
